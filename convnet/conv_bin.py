4318d93e2529f29c995079e2d9d2fed9d5a1b4f0
Ronnypetson <rsronnypetson4@gmail.com>
2017-10-21 20:19:47 -0300

    fixed missing activation on last layer

--- convnet/conv_bin.py
+++ convnet/conv_bin.py
@@ -5,37 +5,31 @@ import numpy as np
 import os, random
 
 img_dir = '../../converted/segmented/cropped/'
-trem_dir = 'TREMULOUS/'
-thorpe_dir = 'Thorpe/'
+char_dir = ['TREMULOUS/','Thorpe/']
+vowel_dir = ['a/','e/','o/','u/']
 img_dim = 20
 batch_size = 16
 learning_rate = 0.001
 num_steps = 1000
 num_epochs = 5
 dropout = 0.75
-classes = {0:'a/',1:'e/',2:'o/',3:'u/'}
+num_classes = 2
 
 def get_batch():    # X: (batch_size,28,28), Y: (batch_size,2)
-    trem_size = batch_size/2
-    thorpe_size = batch_size-trem_size
     X = []
     Y = []
-    for i in range(trem_size):  # trem: [1,0], thorpe: [0,1]
-        fl_loc = img_dir+trem_dir+classes[i%len(classes)]
+    for i in range(batch_size):  # trem: [1,0], thorpe: [0,1]
+        class_ = random.randint(0,num_classes-1)
+        vowel = random.randint(0,3)
+        fl_loc = img_dir+char_dir[class_]+vowel_dir[vowel]
         fl = random.choice(os.listdir(fl_loc))
         #print(fl_loc+fl)
         img = cv2.imread(fl_loc+fl,0)
         img = cv2.resize(img,(img_dim,img_dim))
         X.append(img)
-        Y.append([1.0,0.0])
-    for i in range(thorpe_size):
-        fl_loc = img_dir+thorpe_dir+classes[i%len(classes)]
-        fl = random.choice(os.listdir(fl_loc))
-        #print(fl_loc+fl)
-        img = cv2.imread(fl_loc+fl,0)
-        img = cv2.resize(img,(img_dim,img_dim))
-        X.append(img)
-        Y.append([0.0,1.0])
+        y_ = [0.0,0.0]
+        y_[class_] = 1.0
+        Y.append(y_)
     return X,Y
 
 def bin1(X):
@@ -50,10 +44,11 @@ def bin1(X):
     fc = tf.contrib.layers.flatten(c2)
     fc = tf.layers.dense(fc,200)
     fc = tf.layers.dropout(fc,rate=dropout)
-    return tf.layers.dense(fc,2)
+    fc2 = tf.layers.dense(fc,num_classes)
+    return tf.nn.softmax(fc2)
 
 X = tf.placeholder(tf.float32,shape=(None,img_dim,img_dim))
-Y = tf.placeholder(tf.float32,shape=(None,2))
+Y = tf.placeholder(tf.float32,shape=(None,num_classes))
 
 loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(Y,bin1(X)))
 train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
@@ -72,10 +67,9 @@ with tf.Session() as sess:
             _,loss_,acc_,eq_ = sess.run([train,loss,acc,eq],feed_dict={X:x_,Y:y_})
             s_loss += loss_
             s_acc += acc_
-            #acc_ = sess.run(acc,feed_dict={X:x_,Y:y_})
             if i%100 == 0:
                 print('loss: ',s_loss/100,'accuracy: ',s_acc/100)
-                print(eq_)
+                #print(eq_)
                 s_loss = 0.0
                 s_acc = 0.0
 
