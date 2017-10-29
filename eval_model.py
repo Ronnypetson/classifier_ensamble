2c91dbaae75078efa349f19d3f04a232d988e1fb
Ronnypetson <rsronnypetson4@gmail.com>
2017-10-29 16:42:09 -0300

    added evol model

---
+++ eval_model.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+import tensorflow as tf
+import cv2
+import numpy as np
+import os, random
+
+model_loc = '/checkpoint/conv_vowel/TREMULOUS/' # cl_16_1000_model.ckpt.meta
+
