e1e80fcf48f7f7997994957929532adbccaca22a
Ronnypetson <rsronnypetson4@gmail.com>
2017-11-03 17:11:08 -0300

    fixing inefficiency

--- tree_test.py
+++ tree_test.py
@@ -5,7 +5,6 @@ import itertools
 import os
 import numpy
 import eval_model as em #
-import tensorflow as tf
 
 from deap import algorithms
 from deap import base
@@ -14,13 +13,6 @@ from deap import tools
 from deap import gp
 from scoop import futures
 
-# defined a new primitive set for strongly typed GP
-num_classifiers = 4
-num_classes = 4
-#
-#
-#pset = gp.PrimitiveSetTyped("MAIN",list,list,"IN")   # itertools.repeat
-pset = gp.PrimitiveSet("MAIN",4)    # 2
 #
 def listify(a):
     if type(a) is list:
@@ -57,12 +49,38 @@ def mul_(a,c):
         s.append(e*c)
     return s
 
+def eval_mod(individual):
+    # Transform the tree expression in a callable function
+    func = toolbox.compile(expr=individual)
+    ens_output = []
+    for i in range(len(models_output[0])):
+        res = func(models_output[0][i],models_output[1][i],models_output[2][i],models_output[3][i])
+        if res is None or type(res) is float:
+            return 0.0,
+        ens_output.append(res)
+    # Compute ensemble accuracy
+    acc = 0.0
+    for i in range(len(ens_output)):
+        c1 = numpy.argmax(ens_output[i])
+        c2 = numpy.argmax(t_y[i])
+        if c1 == c2:
+            acc += 1.0
+    print(str(individual))
+    return acc/len(ens_output),
+
+# defined a new primitive set for strongly typed GP
+num_classifiers = 4
+num_classes = 4
+data_dir = '../converted/segmented/cropped/test/TREMULOUS/'
+model_dir = '/checkpoint/conv_vowel/TREMULOUS/'
+model_fn = ['fc_16_5000_model.ckpt','fc_64_5000_model.ckpt','cl_16_5000_model.ckpt','cl_64_5000_model.ckpt']
+#
+#
+#pset = gp.PrimitiveSetTyped("MAIN",list,list,"IN")   # itertools.repeat
+pset = gp.PrimitiveSet("MAIN",4)    # 2
+
 pset.addPrimitive(add_, 2)
-#pset.addPrimitive(operator.sub, [float,float], float)
 pset.addPrimitive(mul_, 2)
-#pset.addPrimitive(max, [float,float], float)
-# terminals
-#pset.addTerminal(False, bool)
 pset.addTerminal(0.0)
 pset.addTerminal(0.1)
 pset.addTerminal(0.2)
@@ -84,58 +102,28 @@ toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.ex
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
-def eval_mod(individual):
-    # Transform the tree expression in a callable function
-    func = toolbox.compile(expr=individual)
-    data_dir = '../converted/segmented/cropped/test/TREMULOUS/'
-    model_dir = '/checkpoint/conv_vowel/TREMULOUS/'
-    #model_fn = []
-    #for fn in os.listdir(model_dir):
-        #if len(fn) >= 5 and fn[-5:] == '.meta':
-            #print(fn)
-            #model_fn.append(fn[:-5])
-    # 'fc_16_5000_model.ckpt','fc_64_5000_model.ckpt'
-    # 'cl_16_5000_model.ckpt','cl_64_5000_model.ckpt',
-    model_fn = ['fc_16_5000_model.ckpt','fc_64_5000_model.ckpt','cl_16_5000_model.ckpt','cl_64_5000_model.ckpt']
-    t_x,t_y = em.get_test(data_dir)
-    models_output = [em.eval(t_x,model_dir+model_fn[i],data_dir).tolist() for i in range(num_classifiers)]
-    ens_output = []
-    for i in range(len(models_output[0])):
-        res = func(models_output[0][i],models_output[1][i],models_output[2][i],models_output[3][i])
-        if res is None or type(res) is float:
-            return 0.0,
-        ens_output.append(res)
-    # Compute ensemble accuracy
-    acc = 0.0
-    for i in range(len(ens_output)):
-        c1 = numpy.argmax(ens_output[i])
-        c2 = numpy.argmax(t_y[i])
-        if c1 == c2:
-            acc += 1.0
-    return acc/len(ens_output),
-
 toolbox.register("evaluate",eval_mod)
 toolbox.register("select", tools.selTournament,tournsize=2)    # 3
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
 toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
 
-def main():
-    #random.seed(10)
-    pop = toolbox.population(n=20)  # 100
-    hof = tools.HallOfFame(1)
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean)
-    stats.register("std", numpy.std)
-    stats.register("min", numpy.min)
-    stats.register("max", numpy.max)
-    
-    algorithms.eaSimple(pop, toolbox, 0.2, 0.1, 20, stats, halloffame=hof)  # 40
-    for t in hof:
-        print(str(t),eval_mod(t))
-    
-    return pop, stats, hof
+#
+t_x, t_y = em.get_test(data_dir)
+models_output = [em.eval(t_x,model_dir+model_fn[i],data_dir).tolist() for i in range(num_classifiers)]
+pop = toolbox.population(n=40)  # 100
+hof = tools.HallOfFame(1)
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("avg", numpy.mean)
+stats.register("std", numpy.std)
+stats.register("min", numpy.min)
+stats.register("max", numpy.max)
 
-if __name__ == "__main__":
-    main()
+algorithms.eaSimple(pop, toolbox, 0.2, 0.1, 40, stats, halloffame=hof)  # 40
+for t in hof:
+    print(str(t),eval_mod(t))
+
+
+#if __name__ == "__main__":
+#    main()
 
